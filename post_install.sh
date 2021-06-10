56698273f417d6e2c3b8013079776329f006f674
Martin Skalický <skalima7@fit.cvut.cz>
2021-06-10 22:17:08 +0200

    Prepare Platform

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
 # Create user 'tetris'
-pw add user -n tetris -c Tetris -s /bin/csh -m
+# pw add user -n tetris -c Tetris -s /bin/csh -m
 
 # Configure shellinabox
-fetch -o /home/tetris/white-on-black.css https://raw.githubusercontent.com/shellinabox/shellinabox/master/shellinabox/white-on-black.css
-sysrc shellinaboxd_enable=YES
-sysrc shellinaboxd_flags="--css /home/tetris/white-on-black.css -t -s /:tetris:tetris:HOME:bastet"
+# fetch -o /home/tetris/white-on-black.css https://raw.githubusercontent.com/shellinabox/shellinabox/master/shellinabox/white-on-black.css
+# sysrc shellinaboxd_enable=YES
+# sysrc shellinaboxd_flags="--css /home/tetris/white-on-black.css -t -s /:tetris:tetris:HOME:bastet"
 
 # Start the service
-service shellinaboxd start
+# service shellinaboxd start
+
+sysrc -f /etc/rc.conf nginx_enable="YES"
+sysrc -f /etc/rc.conf rabbitmq_enable="YES"
+sysrc -f /etc/rc.conf mongod_enable="YES"
+
+service nginx start
+service rabbitmq start
+service mongod start
