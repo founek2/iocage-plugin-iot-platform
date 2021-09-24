14e6d7418806c80830775e4be3e926dad0e58851
Martin Skalický <skalima7@fit.cvut.cz>
2021-09-24 21:41:39 +0200

    Add bots

--- post_install.sh
+++ post_install.sh
@@ -8,15 +8,17 @@
 # sysrc shellinaboxd_enable=YES
 # sysrc shellinaboxd_flags="--css /home/tetris/white-on-black.css -t -s /:tetris:tetris:HOME:bastet"
 
+git clone https://github.com/founek2/IOT-Platforma-bots.git /home/IOT-Platform-bots
+
 # Start the service
 # service shellinaboxd start
 
-sysrc -f /etc/rc.conf nginx_enable="YES"
-sysrc -f /etc/rc.conf rabbitmq_enable="YES"
-sysrc -f /etc/rc.conf mongod_enable="YES"
+# sysrc -f /etc/rc.conf nginx_enable="YES"
+# sysrc -f /etc/rc.conf rabbitmq_enable="YES"
+# sysrc -f /etc/rc.conf mongod_enable="YES"
 sysrc -f /etc/rc.conf pm2_enable="YES"
 
-service nginx start
-service rabbitmq start
-service mongod start
+# service nginx start
+# service rabbitmq start
+# service mongod start
 service pm2 start
