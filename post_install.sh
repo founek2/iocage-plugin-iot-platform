03e083d8ad05eab9a4d67610ded626b71e05875c
Martin Skalický <skalima7@fit.cvut.cz>
2021-09-25 22:00:47 +0200

    Update

--- post_install.sh
+++ post_install.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 # Create user 'tetris'
-pw add user -n freebsd -c Freebsd -s /bin/csh -m
+pw add user -n deployer -c Deployer -s /bin/csh -m
 yarn global add pm2
 
 # Configure shellinabox
 # fetch -o /home/tetris/white-on-black.css https://raw.githubusercontent.com/shellinabox/shellinabox/master/shellinabox/white-on-black.css
 # sysrc shellinaboxd_enable=YES
 # sysrc shellinaboxd_flags="--css /home/tetris/white-on-black.css -t -s /:tetris:tetris:HOME:bastet"
-
-git clone https://github.com/founek2/IOT-Platforma-bots.git /home/freebsd/IOT-Platform-bots
 
 # Start the service
 # service shellinaboxd start
@@ -23,3 +21,7 @@ sysrc -f /etc/rc.conf pm2_enable="YES"
 # service rabbitmq start
 # service mongod start
 service pm2 start
+
+su - deployer
+git clone https://github.com/founek2/IOT-Platforma-bots.git IOT-Platform-bots
+cd IOT-Platform-bots && yarn
